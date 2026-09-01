2d80df171d07fea38edaebdd7ad5d9a327b96a76
Anjali Keshri <anjalike16@gmail.com>
2026-09-02 00:08:49 +0530

    Time: 4 ms (100%), Space: 46 MB (52.81%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int n=x, rev=0;
+        if(x<0)
+        {
+            return false;
+        }
+        while(x!=0)
+        {
+            rev=rev*10 + x%10;
+            x/=10;
+        }
+        return (n==rev);
+    }
+}
